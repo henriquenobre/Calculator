49b48d509d78e1e3c409fd9d760ef47878b5e15a
Henrique <henriquenobr@gmail.com>
2021-11-25 10:03:49 -0300

    Updates

---
+++ precache-manifest.74fc646e8c4567261be27696237b236c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19a6d505a83d9937d361",
+    "url": "/Calculator/static/css/main.761c2511.chunk.css"
+  },
+  {
+    "revision": "19a6d505a83d9937d361",
+    "url": "/Calculator/static/js/main.e5efba66.chunk.js"
+  },
+  {
+    "revision": "c2a97d8d426c7554df3f",
+    "url": "/Calculator/static/js/runtime~main.37007537.js"
+  },
+  {
+    "revision": "3d8fdcc8ed546ad00911",
+    "url": "/Calculator/static/js/2.0727302f.chunk.js"
+  },
+  {
+    "revision": "d26ea6249e448cd2a6beb88f48903431",
+    "url": "/Calculator/index.html"
+  }
+];
